8d9dba0fa6d46fbb8f8c8b3a3720adabb9114fff
Slava Borysyuk <borysyy@sunypoly.edu>
2023-10-27 14:10:51 -0400

    GHP3 Work

--- GHP3/GHP3.cpp
+++ GHP3/GHP3.cpp
@@ -9,3 +9,64 @@ Description:
 
 
 */
+#include <iostream>
+
+using namespace std;
+
+class Square
+{
+public:
+    Square(const char *letterArray, int size)
+    {
+        if (size > 0)
+        {
+            letters = new char[size];
+            for (int i = 1; i <= size; i++)
+            {
+                letters[i] = letterArray[i];
+            }
+        }
+        letterCount = size;
+    }
+
+    void displayLettersSquare()
+    {
+        int length = letterCount * 2 - 1;
+    
+        for(int i = 0; i < (length / 2) + 1; i++)
+        {
+            
+
+        }
+    
+       
+    }
+  
+
+private:
+    char *letters;
+    int letterCount;
+};
+
+
+int main(void)
+{
+
+    int letter_amount = 0;
+
+    cout << "How many letters are you going to use: ";
+    cin >> letter_amount;
+
+    char letters[letter_amount];
+
+    for (int i = 1; i <= letter_amount; i++)
+    {
+        cout << "Letter " << i << ": ";
+        cin >> letters[i];
+    }
+
+    Square mySquare(letters, letter_amount);
+    mySquare.displayLettersSquare();
+
+    return 0;
+}
