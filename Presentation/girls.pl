d8dc758d6352ec3de51c1de70e28af35f6b73f78
Slava Borysyuk <borysyy@sunypoly.edu>
2023-11-08 00:28:59 -0500

    Presentation Work

---
+++ Presentation/girls.pl
@@ -0,0 +1,10 @@
+girl(priya).
+girl(tiyasha).
+girl(jaya).
+
+can_cook(priya).
+can_cook(jaya).
+can_cook(tiyasha).
+
+likes(priya,jaya) :- can_cook(jaya).
+likes(priya,tiyasha) :- can_cook(tiyasha).
