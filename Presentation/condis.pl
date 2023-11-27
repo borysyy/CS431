c5c7d6e45d9f85cd87a562585d4ff87f9344b54b
borysyy <borysyy@sunypoly.edu>
2023-11-27 13:38:03 -0500

    Work

---
+++ Presentation/condis.pl
@@ -0,0 +1,12 @@
+parent(john,bob).
+parent(lili,bob).
+
+male(john).
+female(lili).
+
+% Conjunction Logic
+father(X,Y) :- parent(X,Y),male(X).
+mother(X,Y) :- parent(X,Y),female(X).
+
+% Disjunction Logic
+child_of(X,Y) :- father(X,Y);mother(X,Y).
